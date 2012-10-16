8754fbfeeedc76b331f5d5beefd32f1ac7141556
Thomas BOHL <thomas.bohl@gmail.com>
2012-10-16 15:14:39 -0400

    tests

--- examples/android/androidImageExample/src/testApp.cpp
+++ examples/android/androidImageExample/src/testApp.cpp
@@ -18,38 +18,3 @@ void testApp::draw(){
 	image.draw(20,20);
 }
 
-//--------------------------------------------------------------
-void testApp::keyPressed  (int key){ 
-	
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){ 
-	
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-	ofLog(OF_LOG_WARNING,"%i,%i",x,y);
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-	
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-	
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
-}
-
